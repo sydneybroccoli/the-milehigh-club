d275c8ba37c2a0ab0ef9e3c8215076b53713f761
Rich Cherry <rich.m.cherry@gmail.com>
2020-03-02 18:17:37 +0800

    Add transactions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'aircrafts/index'
-  get 'aircrafts/show'
-  get 'aircrafts/new'
-  get 'aircrafts/create'
-  get 'aircrafts/edit'
-  get 'aircrafts/update'
-  get 'aircrafts/delete'
+  # get 'aircrafts/index'
+  # get 'aircrafts/show'
+  # get 'aircrafts/new'
+  # get 'aircrafts/create'
+  # get 'aircrafts/edit'
+  # get 'aircrafts/update'
+  # get 'aircrafts/delete'
   root to: 'pages#home'
+
+  resources :aircrafts do
+    resources :transactions, only: [ :create, :new ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
