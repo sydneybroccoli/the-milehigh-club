545277f061afe9c2649fd7131a83de4ea85d2b2e
Joseph Driver <joseph.driver0@gmail.com>
2020-03-03 11:30:20 +0800

    added devise, fixed routes, fixed db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :aircrafts, dependent: :destroy
   has_many :transactions
   has_many :reviews
