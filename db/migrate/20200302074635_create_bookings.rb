4199b75fdb6b35a0984b7670e569c4819b891dda
Yassin Elsir <yassinmelsir@gmail.com>
2020-03-03 15:26:42 +0800

    updated default values of booking schema

--- db/migrate/20200302074635_create_bookings.rb
+++ db/migrate/20200302074635_create_bookings.rb
@@ -1,11 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-      t.string :booking_type
+      t.string :booking_type, default: 'rental'
       t.datetime :start_date
       t.datetime :end_date
-      t.boolean :seen
-      t.boolean :confirm
+      t.boolean :seen, default: false
+      t.boolean :confirm, default: false
       t.float :final_price
       t.references :user, foreign_key: true
       t.references :aircraft, foreign_key: true
