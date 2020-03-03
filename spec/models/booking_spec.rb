b33d71983be9df77a4fbb4f6025a78da89850b9e
Sydney Barovsky <sbarovsky@gmail.com>
2020-03-03 15:17:37 +0800

    set up for remaining models. to be continued

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -2,36 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  buyer = User.create(
-    username: 'joe',
-    password: 'password',
-    email: 'joe0@gmail.com'
-    )
-  owner = User.create(
-    username: 'sydney',
-    password: 'password',
-    email: 'sbarovsky@gmail.com'
-    )
-  aircraft = Aircraft.create(
-      make: 'make',
-      model: 'model',
-      location: 'location',
-      price: 1_000_000,
-      capacity: 100,
-      hours: 10_000,
-      year: 2020,
-      description: 'description',
-      user_id: owner.id
-    )
 
-  valid_attributes = {
-    booking_type: 'sale',
-    start_date: Date.today,
-    end_date: Date.tomorrow,
-    seen: true,
-    confirm: true,
-    user_id: buyer.id,
-    aircraft_id: aircraft.id,
-    final_price: aircraft.price * (Date.tomorrow - Date.today)
-  }
+  context 'booking_type...' do
+  end
+
+  context 'start_date....' do
+  end
+
+  context 'end_date....' do
+  end
+
+  context 'seen...' do
+  end
+
+  context 'confirm...' do
+  end
+
+  context 'user_id (buyer)...' do
+  end
+
+  context 'aircraft_id...' do
+  end
+
+  context 'final_price...' do
+  end
+
+  context 'has many/belongs to...' do
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'content' do
+  end
+
+  context 'rating' do
+  end
+
+  context 'booking_id' do
+  end
+
+  context 'user_id (buyer)' do
+  end
 end
