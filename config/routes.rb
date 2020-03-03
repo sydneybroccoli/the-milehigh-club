e71a88978e0f06778482ee90154af126210b1c8c
Joseph Driver <joseph.driver0@gmail.com>
2020-03-03 10:22:29 +0800

    hero banner done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   get 'aircrafts/index'
   get 'aircrafts/show'
   get 'aircrafts/new'
@@ -6,6 +7,5 @@ Rails.application.routes.draw do
   get 'aircrafts/edit'
   get 'aircrafts/update'
   get 'aircrafts/delete'
-  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
