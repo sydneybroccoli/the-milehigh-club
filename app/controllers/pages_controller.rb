08b077f2d3f8f2fe0e52d2dc89a50defa857c053
Rich Cherry <rich.m.cherry@gmail.com>
2020-03-04 14:16:58 +0800

    Added map showing all aircraft to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @aircrafts = Aircraft.geocoded #returns flats with coordinates
+    @markers = @aircrafts.map do |aircraft|
+      {
+        lat: aircraft.latitude,
+        lng: aircraft.longitude
+      }
+    end
+
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,7 @@
 import "bootstrap";
 import flatpickr from "flatpickr";
-
+import 'mapbox-gl/dist/mapbox-gl.css';
 import "../plugins/init_flatpickr.js"
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
