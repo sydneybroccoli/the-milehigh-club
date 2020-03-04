b3415bfd1f2eac7cb29f115ed8df702660c556a6
Sydney Barovsky <sbarovsky@gmail.com>
2020-03-04 13:46:10 +0800

    working update form for user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-<<<<<<< HEAD
   end
 
   def edit
@@ -10,22 +9,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    edit
-    @user.update(user_params)
-    redirect_to user_path(@user)
-=======
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
     @aircraft = Aircraft.new
->>>>>>> 67d894d8c0c6782563134c9ff2523321b6b0b319
   end
 
   private
 
   def user_params
-<<<<<<< HEAD
     params.require(:user).permit(:email, :first_name, :last_name, :favorite, :content, :photo)
-
-=======
-    params.require(:user).permit(:email)
->>>>>>> 67d894d8c0c6782563134c9ff2523321b6b0b319
   end
 end
