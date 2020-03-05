515269cd2c0455cd603e04e423668faca1373107
Yassin Elsir <yassinmelsir@gmail.com>
2020-03-05 11:20:29 +0800

    fixed user booking to booking details routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ Rails.application.routes.draw do
 
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  
+
   resources :users, :only => [:show, :edit, :update]
-  
+
   root to: 'pages#home'
 
   resources :aircrafts do
-    resources :bookings, only: [ :create, :new, :show ]
+    resources :bookings, only: [ :create, :new]
     resources :reviews, only: [ :create, :new ]
   end
+  resources :bookings, only: [ :show ]
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
