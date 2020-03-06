3d8e3fc6dd82066252a0423b2f44a704828813c9
Sydney Barovsky <sbarovsky@gmail.com>
2020-03-06 17:37:01 +0800

    comment in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -299,10 +299,10 @@ i = 0
     puts "\ta #{tmp[:make]} #{tmp[:model]} has been listed!"
 
     aircraft.save!
-    until aircraft.geocoded?
-      aircraft[:location] = Faker::Address.time_zone
-    end
-    aircraft.save!
+    # until aircraft.geocoded?
+    #   aircraft[:location] = Faker::Address.time_zone
+    # end
+    # aircraft.save!
 
     aircrafts_array << aircraft
 
