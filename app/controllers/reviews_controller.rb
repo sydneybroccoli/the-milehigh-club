1c0c46a959a00642e7d5febd3c261423ad212708
Joseph Driver <joseph.driver0@gmail.com>
2020-03-06 12:41:09 +0800

    user bookings

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,15 +2,13 @@
 
   def new
     @review = Review.new
-    @booking = Booking.find(params[:transaction_id])
+    @aircraft = Aircraft.find(params[:aircraft_id])
   end
 
   def create
-    @booking = Booking.find(params[:transaction_id])
     @review = Review.new(review_params)
-    # @review.booking = @booking
-    @review.booking = current_user.bookings.last
-    if review.save
+    @aircraft = Aircraft.find(params[:aircraft_id])
+    if @review.save!
       redirect_to user_aircraft_path(@review.booking.aircraft)
     else
       render 'new'
@@ -20,6 +18,6 @@
   private
 
   def review_params
-    params.require(:review).require(:rating, :content)
+    params.require(:review).permit(:rating, :content)
   end
 end
