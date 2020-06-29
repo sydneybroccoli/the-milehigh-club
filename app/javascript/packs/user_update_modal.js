54b73fdd374a05cb0d84f3487b106537b527788d
Sydney Barovsky <sbarovsky@gmail.com>
2020-06-29 15:54:33 +0800

    all "update..." lines are links to open modal

---
+++ app/javascript/packs/user_update_modal.js
@@ -0,0 +1,13 @@
+export const userUpdateModal = () => {
+  const btns = document.querySelectorAll('.update-user');
+  const update = document.querySelector('#user-update-btn');
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+      // PREVENT REFRESH ON CLICK
+      e.preventDefault();
+      // OPEN MODAL ON CLICK
+      update.click();
+    });
+  });
+}
